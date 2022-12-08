34fb2057bea8be73cd22ab3df24df4fc07dbbf82
Tomasz Kogut <almendar@gmail.com>
2022-12-08 15:42:18 +0100

    Reformat day8

--- day8.py
+++ day8.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass, field
 
 from util import run_day
+
+
 @dataclass
 class Tree:
     height: int
@@ -20,135 +22,140 @@ class Tree:
     def __repr__(self) -> str:
         return f"{self.height}:{self.is_visible()}"
 
-@dataclass
-class Plane:
-    forest: list[list[Tree]]
 
-    def __getitem__(self, key:int):
-        return self.forest[key]
-    
-    @property
-    def x_len(self) ->int:
-        return len(self.forest[0])
+Plane = list[list[Tree]]
 
-    @property
-    def y_len(self) ->int:
-        return len(self.forest)
+# @dataclass
+# class Plane:
+#     forest:
+
+#     def __getitem__(self, key: int):
+#         return self.forest[key]
+
+#     @property
+#     def x_len(self) -> int:
+#         return len(self.forest[0])
+
+#     @property
+#     def y_len(self) -> int:
+#         return len(self.forest)
 
 
-    def scenicScore(self, x :int, y :int)->int:
-        height = self[y][x].height
-        
-        # right
-        right_sum = 0
-        for xi in range(x+1, self.x_len):
-            if height > self[y][xi].height:
-                right_sum += 1
-            else:
-                right_sum += 1
-                break
-        
-        left_sum = 0
-        #left
-        for xi in range(x-1, -1,-1):
-            if height > self[y][xi].height:
+def scenic_score(plane: Plane, x: int, y: int) -> int:
+    height = plane[y][x].height
+    x_len = len(plane[0])
+    y_len = len(plane)
 
-                left_sum+=1
-            else:
-                left_sum+=1
-                break
-        
-        bottom_sum = 0
-        #bottom
-        for yi in range(y+1, self.y_len):
-            if height > self[yi][x].height:
+    # right
+    right_sum = 0
+    for xi in range(x + 1, x_len):
+        if height > plane[y][xi].height:
+            right_sum += 1
+        else:
+            right_sum += 1
+            break
 
-                bottom_sum +=1
-            else:
-                bottom_sum +=1
-                break
+    # left
+    left_sum = 0
+    for xi in range(x - 1, -1, -1):
+        if height > plane[y][xi].height:
+            left_sum += 1
+        else:
+            left_sum += 1
+            break
 
-        #up
-        up_sum = 0
-        for yi in range(y-1,-1,-1):
-            if height > self[yi][x].height:
+    # down
+    down = 0
+    for yi in range(y + 1, y_len):
+        if height > plane[yi][x].height:
+            down += 1
+        else:
+            down += 1
+            break
 
-                up_sum += 1
-            else:
-                up_sum +=1
-                break
-        return left_sum * right_sum * up_sum * bottom_sum
+    # up
+    up_sum = 0
+    for yi in range(y - 1, -1, -1):
+        if height > plane[yi][x].height:
 
-    def count_visible(self) -> int:
-        y_len = self.y_len
-        x_len = self.x_len
-        
-        # left to right
+            up_sum += 1
+        else:
+            up_sum += 1
+            break
+    return left_sum * right_sum * up_sum * down
+
+
+def count_visible(plane: Plane) -> int:
+    x_len = len(plane[0])
+    y_len = len(plane)
+
+    # left to right
+    for y in range(1, y_len - 1):
+        curr_max = plane[y][0].height
+        for x in range(1, x_len - 1):
+            curr = plane[y][x]
+            if curr_max >= curr.height:
+                curr.left_visible = False
+            if curr.height > curr_max:
+                curr_max = curr.height
+
+    # right to left
+    for y in range(1, y_len - 1):
+        curr_max = plane[y][x_len - 1].height
+        for x in range(x_len - 2, 0, -1):
+            curr = plane[y][x]
+            if curr_max >= curr.height:
+                curr.right_visible = False
+            if curr.height > curr_max:
+                curr_max = curr.height
+
+    # top to bottom
+    for x in range(1, x_len - 1):
+        curr_max = plane[0][x].height
         for y in range(1, y_len - 1):
-            curr_max = self[y][0].height
-            for x in range(1, x_len-1):
-                curr = self[y][x]
-                if curr_max >= curr.height:
-                    curr.left_visible = False
-                if curr.height > curr_max:
-                    curr_max = curr.height
+            curr = plane[y][x]
+            if curr_max >= curr.height:
+                curr.up_visible = False
 
-        # right to left
-        for y in range(1, y_len - 1):
-            curr_max = self[y][x_len-1].height
-            for x in range(x_len - 2, 0, -1):
-                curr = self[y][x]
-                if curr_max >= curr.height:
-                    curr.right_visible = False
-                if curr.height > curr_max:
-                    curr_max = curr.height
-                
+            if curr.height > curr_max:
+                curr_max = curr.height
 
-        # top to bottom
-        for x in range(1, x_len-1):
-            curr_max = self[0][x].height
-            for y in range(1, y_len-1):
-                curr = self[y][x]
-                if curr_max >= curr.height:
-                    curr.up_visible = False
-            
-                if curr.height > curr_max:
-                    curr_max = curr.height
-                
+    # bottom to top
+    for x in range(1, y_len - 1):
+        curr_max = plane[y_len - 1][x].height
+        for y in range(y_len - 2, 0, -1):
+            curr = plane[y][x]
+            if curr_max >= curr.height:
+                curr.down_visible = False
+            if curr.height > curr_max:
+                curr_max = curr.height
 
-        # bottom to top
-        for x in range(1, y_len-1):
-            curr_max = self[y_len-1][x].height
-            for y in range(y_len - 2, 0, -1):
-                curr = self[y][x]
-                if curr_max >= curr.height:
-                    curr.down_visible = False
-                if curr.height > curr_max:
-                    curr_max = curr.height
-
-        count = 0
-        for y in range(0, y_len):
-            for x in range(0, len(self[y])):
-                if self[y][x].is_visible():
-                    count += 1
-        return count
+    count = 0
+    for y in range(0, y_len):
+        for x in range(0, len(plane[y])):
+            if plane[y][x].is_visible():
+                count += 1
+    return count
 
 
 def read_input(input: str) -> Plane:
     with open(input) as f:
         lines = [line.strip() for line in f]
-        return Plane( [[Tree(int(nr)) for nr in line] for line in lines])
+        return Plane([[Tree(int(nr)) for nr in line] for line in lines])
+
 
 def part1(input):
     plane = read_input(input)
-    return plane.count_visible()
+    return count_visible(plane)
+
 
 def part2(input):
     plane = read_input(input)
-    acc = []
-    for y in range(0,plane.y_len):
-        for x in range(0, plane.x_len):
-           acc.append(plane.scenicScore(x,y))
-    return max(acc)
+    x_len = len(plane[0])
+    y_len = len(plane)
+    return max(
+        [scenic_score(plane, x, y) for y in range(0, y_len) for x in range(0, x_len)]
+    )
 
-run_day(8,part1, part2)
+
+run_day(8, part1, part2)
