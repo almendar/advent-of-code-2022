479844f45bc72cd14eec7a93a8df2629170a5d83
Tomasz Kogut <almendar@gmail.com>
2022-12-15 12:38:41 +0100

    Reafctor day15

--- day15.py
+++ day15.py
@@ -1,25 +1,30 @@
 from typing import NamedTuple, FrozenSet, Dict, NewType, Tuple, Set
-from itertools import groupby
-
+from dataclasses import dataclass, field
 
 sample_txt = "input/day15-sample.txt"
 input_txtx = "input/day15-input.txt"
 
 
-class Coor(NamedTuple):
+class Coord(NamedTuple):
     x: int
     y: int
 
 
-class Sensor(Coor):
-    pass
+def manhattan(p1: Coord, p2: Coord) -> int:
+    return abs(p1.x - p2.x) + abs(p1.y - p2.y)
 
 
-class Beacon(Coor):
-    pass
+@dataclass
+class Sensor:
+    loc: Coord
+    beacon: Coord
+    max_reach: int = field(init=False)
 
+    def __post_init__(self):
+        self.max_reach = manhattan(self.loc, self.beacon)
 
-ManhattanDistance = NewType("ManhattanDistance", int)
+    def covers(self, c: Coord) -> bool:
+        return manhattan(self.loc, c) <= self.max_reach
 
 
 def parse_line(l):
@@ -28,7 +33,7 @@ def parse_line(l):
     sy = int(sy[:-1].split("=")[1])
     bx = int(bx[:-1].split("=")[1])
     by = int(by.split("=")[1])
-    return (Sensor(sx, sy), Beacon(bx, by))
+    return Coord(sx, sy), Coord(bx, by)
 
 
 def read_input(path):
@@ -36,41 +41,13 @@ def read_input(path):
         return [parse_line(l) for l in f]
 
 
-def manhattan(p1: Coor, p2: Coor) -> ManhattanDistance:
-    return ManhattanDistance(abs(p1.x - p2.x) + abs(p1.y - p2.y))
-
-
-def gen_coverage(p: Sensor, d: int):
-
-    acc = set()
-    for yi in range(p.y - d, p.y + d + 1):
-        delta = abs(yi - p.y)
-        for xi in range(p.x - d + delta, p.x + d + 1 - delta):
-            if (xi, yi) != p:
-                acc.add((xi, yi))
-    return acc
-
-
-def print_coverage(center, coverage):
-    min_x = min(map(lambda x: x[0], coverage))
-    max_x = max(map(lambda x: x[0], coverage))
-    min_y = min(map(lambda x: x[1], coverage))
-    max_y = max(map(lambda x: x[1], coverage))
-    for yi in range(min_y, max_y + 1):
-        for xi in range(min_x, max_x + 1):
-            if (xi, yi) in coverage:
-                print("#", end="")
-            else:
-                print(".", end="")
-        print("")
-
-
-def coverage_at_y(s: Sensor, ray: ManhattanDistance, y: int):
-    from_y = abs(y - s.y)
-    if from_y > ray:
+def coverage_at_y(s: Sensor, y: int):
+    loc = s.loc
+    from_y = abs(y - loc.y)
+    if from_y > s.max_reach:
         return set()
-    x_ray = ray - from_y
-    return [(i, y) for i in range(s.x - x_ray, s.x + x_ray + 1)]
+    x_ray = s.max_reach - from_y
+    return {(i, y) for i in range(loc.x - x_ray, loc.x + x_ray + 1)}
 
 
 def part1(input, y_pos):
@@ -78,12 +55,22 @@ def part1(input, y_pos):
     trtrt = set()
     for ft in input_data:
         s, b = ft
-        dis = manhattan(s, b)
-        trtrt |= set(coverage_at_y(s, dis, y_pos))
+        trtrt |= coverage_at_y(Sensor(s, b), y_pos)
         if b.y == y_pos:
             trtrt.remove(b)
     return len(trtrt)
 
 
-part1(sample_txt, 10)
-part1(input_txtx, 2000000)
+def part2(input):
+    pass
+    # input_data = read_input(input)
+    # trtrt = set()
+    # for ft in input_data:
+    #     s, b = ft
+    #     trtrt |= coverage_at_y(Sensor(s, b), y_pos)
+    #     if b.y == y_pos:
+    #         trtrt.remove(b)
+
+
+print(part1(sample_txt, 10))
+print(part1(input_txtx, 2000000))
