3df62dfbe1030381cc8ef96682695f0dc7b3946e
Tomasz Kogut <almendar@gmail.com>
2022-12-20 15:30:33 +0100

    Day 20 complete

--- day20.py
+++ day20.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Callable
-from util import day_data
+from util import run_day
 
 
 @dataclass
@@ -9,7 +9,6 @@ class Node:
     prev: "Node" = field(init=False)
     next: "Node" = field(init=False)
     original_next: "Node" = field(init=False)
-    sentinel: bool = field(default=False)
 
     def __post_init__(self):
         self.prev = self
@@ -31,12 +30,10 @@ class Node:
         iter = self
         while n > 0:
             iter = iter.next
-            if iter.sentinel:
-                iter = iter.next
             n -= 1
         return iter
 
-    def attach_after(self, n: "Node") -> "Node":
+    def attach_after(self, n: "Node"):
         curr_next = self.next
         curr_next.prev = n
         self.next = n
@@ -44,7 +41,7 @@ class Node:
         n.next = curr_next
         return n
 
-    def attach_before(self, n: "Node") -> "Node":
+    def attach_before(self, n: "Node"):
         curr_prev = self.prev
         curr_prev.next = n
         self.prev = n
@@ -52,18 +49,13 @@ class Node:
         n.prev = curr_prev
         return n
 
-    def deattach(self) -> "Node":
-        if self.sentinel:
-            raise ValueError("deattach of sentinel")
+    def deattach(self):
         prev, next = self.prev, self.next
         prev.next = next
         next.prev = prev
-        return self
 
     def move(self, delta: int):
-        # print("moving", self.value)
         deattach = self
-
         op = None
         if delta == 0:
             return self
@@ -72,55 +64,29 @@ class Node:
         elif delta < 0:
             op = lambda x: x.prev
 
+        deattach.deattach()
         cnt = abs(delta)
         wheere_to_attach = self
         while cnt > 0:
             wheere_to_attach = op(wheere_to_attach)
-            if wheere_to_attach.sentinel:
-                wheere_to_attach = op(wheere_to_attach)
             cnt -= 1
 
-        if deattach == wheere_to_attach:
-            return
-
-        deattach.deattach()
         if delta > 0:
             wheere_to_attach.attach_after(deattach)
-            if wheere_to_attach.next.sentinel:
-                wheere_to_attach = wheere_to_attach.next
         elif delta < 0:
-            if wheere_to_attach.prev.sentinel:
-                wheere_to_attach = wheere_to_attach.prev
             wheere_to_attach.attach_before(deattach)
 
-    def __repr__(self) -> str:
-        if self.sentinel:
-            return "#"
-        else:
-            return str(self.value)
-
-
-@dataclass
-class Chain:
-    sentinel: Node
-
-    def append(self, value: int) -> "Node":
-        return self.sentinel.prev.create(value)
-
-    def print(self):
+    def print_chain(self):
         acc = []
-        node = self.sentinel.next
-        while not node.sentinel:
+        acc.append(self.value)
+        node = self.next
+        while not node == self:
             acc.append(node.value)
             node = node.next
         print(acc)
 
-
-numbers = range(1, 14)
-
-
-def parse_input():
-    pass
+    def __repr__(self) -> str:
+        return str(self.value)
 
 
 def part1(input):
@@ -128,22 +94,19 @@ def part1(input):
     with open(input) as f:
         numbers = [int(i.strip()) for i in f]
 
-    # numbers = [1, 2, -3, 3, -2, 0, 4]
-    sentinel = Node(0, True)
-    chain = Chain(sentinel)
+    head = Node(numbers[0])
+    last = head
+    for i in numbers[1:]:
+        last = last.create(i)
 
-    for i in numbers:
-        chain.append(i)
-
-    iter = sentinel.original_next
-    while not iter.sentinel:
-        # chain.print()
+    head.move(head.value)
+    iter = head.original_next
+    while iter != head:
         iter.move(iter.value)
         iter = iter.original_next
 
-    # chain.print()
-    iter = sentinel.next
-    while not iter.sentinel and iter.value != 0:
+    iter = head
+    while iter.value != 0:
         iter = iter.next
 
     n_1000 = iter.nth(1000).value
@@ -152,7 +115,57 @@ def part1(input):
     return n_1000 + n_2000 + n_3000
 
 
-sample_txt, input_txt = day_data(20)
+def reduce_delta(steps, list_length):
+    if steps == 0:
+        return steps
+    if steps > (list_length - 1):
+        steps = steps % (list_length - 1)
 
-print(part1(sample_txt))
-print(part1(input_txt))
+    if steps < 0:
+        step_abs = abs(steps)
+        if step_abs > (list_length - 1):
+            step_abs = step_abs % (list_length - 1)
+        steps = list_length - 1 - step_abs
+
+    return steps
+
+
+def part2(input):
+    MULTIPLY = 811589153
+
+    numbers = []
+    with open(input) as f:
+        numbers = [int(i.strip()) for i in f]
+
+    head = Node(numbers[0] * MULTIPLY)
+    last = head
+    for i in numbers[1:]:
+        last = last.create(i * MULTIPLY)
+
+    list_length = len(numbers)
+
+    for k in range(10):
+        steps = reduce_delta(head.value, list_length)
+        head.move(steps)
+        iter = head.original_next
+        while iter != head:
+            steps = reduce_delta(iter.value, list_length)
+            iter.move(steps)
+            iter = iter.original_next
+
+    iter = head
+    while iter.value != 0:
+        iter = iter.next
+    n_1000 = iter.nth(1000).value
+    n_2000 = iter.nth(2000).value
+    n_3000 = iter.nth(3000).value
+    return n_1000 + n_2000 + n_3000
+
+
+# sample_txt, input_txt = day_data(20)
+
+run_day(20, part1, part2)
+# print(part1(sample_txt))  # 3
+# print(part1(input_txt))  # 8721
+# print(part2(sample_txt))  # 1623178306
+# print(part2(input_txt))  # 831878881825
